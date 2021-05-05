e57366e1aeff3830a41bf1ab7fa524950e2ebeca
Jerse1 <70332247+Jerse1@users.noreply.github.com>
2021-05-05 22:37:05 +0300

    Create JTweenService.lua

---
+++ JTweenService.lua
@@ -0,0 +1,108 @@
+local RS = game:GetService("RunService");
+local Heartbeat = RS.Heartbeat;
+
+local Easings = require(script.Easings);
+
+local JTweenService = {};
+JTweenService.__index = JTweenService;
+
+local Tween = {};
+Tween.__index = Tween;
+
+local switch_Case = {
+	Color3 = {}
+}
+
+--[[
+	TODO: Make a switch statement instead of using if's;
+	TODO: Add in more Easings & Allow usage of Easing in the tweenInfo array;
+	Fyi, I know this is hard to read;
+]]
+function Tween:updateProperties(Time : number, Duration : number)
+	for i,v in pairs(self.Goals) do
+		if typeof(v[1]) == "Color3" then 
+			local Final = Color3.new(
+				Easings["Linear"](Time,v[1].R,v[2].R,Duration),
+				Easings["Linear"](Time,v[1].G,v[2].G,Duration),
+				Easings["Linear"](Time,v[1].B,v[2].B,Duration));
+			self.Instance[i] = Final;
+		elseif typeof(v[1]) == "Vector3" then
+			local Final = Vector3.new(
+				Easings["Linear"](Time,v[1].X,v[2].X,Duration),
+				Easings["Linear"](Time,v[1].Y,v[2].Y,Duration),
+				Easings["Linear"](Time,v[1].Z,v[2].Z,Duration));
+			self.Instance[i] = Final;
+		elseif typeof(v[1]) == "CFrame" then
+			local Final  = CFrame.new(
+				Easings["Linear"](Time,v[1].X,v[2].X,Duration),
+				Easings["Linear"](Time,v[1].Y,v[2].Y,Duration),
+				Easings["Linear"](Time,v[1].Z,v[2].Z,Duration),
+				Easings["Linear"](Time,v[1].Z,v[2].Z,Duration),
+				)
+		elseif typeof(v[1]) == "number" then
+			self.Instance[i] = Easings["Linear"](Time,v[1],v[2],Duration);
+		end
+	end
+end
+
+function Tween:Update()
+	local Duration = self.tweenInfo[1];
+	if not (tick() - self.startTime < Duration) then
+		self.Connection:Disconnect();
+		self:Destroy();
+		return;
+	end
+	if (tick() - self.startTime < Duration) and self.PlaybackState == Enum.PlaybackState.Playing then
+		self:updateProperties((tick() - self.startTime)/Duration,Duration);
+	end
+end
+
+function Tween:Destroy()
+	if self.Connection then self.Connection:Disconnect(); end
+	self.__mode = "k";
+	setmetatable(self,nil);
+	self = nil;
+end
+
+function Tween:Play()
+	local Duration = self.tweenInfo[1];
+
+	self.startTime = tick();
+	
+	self.PlaybackState = Enum.PlaybackState.Playing;
+	
+	self.Connection = Heartbeat:Connect(function()
+		self:Update();
+	end)
+end
+
+function Tween:Pause()
+	self.PlaybackSate = Enum.PlaybackState.Paused;
+	self.Connection:Disconnect();
+end
+
+function Tween:Cancel()
+	self.PlaybackState = Enum.PlaybackState.Cancelled;
+	self.Connection:Disconnect();
+end
+
+function JTweenService:Create(Instance : Instance, tweenInfo : table, Goals : table)
+	assert(Instance,"[JTweenService] : [1] argument is missing");
+	assert(tweenInfo,"[JTweenService] : [2] argument is missing");
+	assert(Goals,"[JTweenService] : [3] argument is missing");
+
+	for i,v in pairs(Goals) do
+		assert(Instance[i],("[JTweenService] : %s doesn't have a property called \"%s\"."):format(Instance:GetFullName(),i));
+		Goals[i] = {Instance[i],Goals[i]};
+	end
+
+	return setmetatable({
+		PlaybackState = Enum.PlaybackState.Begin,
+		Instance = Instance,
+		tweenInfo = tweenInfo,
+		Goals = Goals 
+
+	},Tween);
+end
+
+return JTweenService;
